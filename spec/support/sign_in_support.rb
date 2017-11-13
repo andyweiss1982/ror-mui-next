bba7020dc6afd0ae906325ecb62bb5877d3f7289
Andy Weiss <andyweiss1982@gmail.com>
2017-11-13 14:14:57 -0500

    user logs in spec

---
+++ spec/support/sign_in_support.rb
@@ -0,0 +1,6 @@
+def sign_in_as(email:, password:)
+  visit root_path
+  fill_in "user_email", with: email
+  fill_in "user_password", with: password
+  click_button "Log In"
+end
---
+++ spec/system/user_logs_in_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "user logs in", type: :system, js: true do
+  context "with a valid email and password" do
+    it "redirects the user to his dashboard" do
+      user = create_user(email: "user@example.com", password: "password")
+      sign_in_as(email: user.email, password: "password")
+      expect(page).to have_content("Signed in successfully.")
+    end
+  end
+  context "with an invalid password" do
+    it "shows the user an error" do
+      sign_in_as(email: "nonexistentuser@example.com", password: "password")
+      expect(page).to have_content("Invalid Email or Password")
+    end
+  end
+  context "without filling the form" do
+    it "shows the user an error" do
+      sign_in_as(email: "", password: "")
+      expect(page).to have_content("Invalid Email or Password")
+    end
+  end
+end
