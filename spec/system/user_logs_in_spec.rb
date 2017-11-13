7fb5450ab5bd1f734a28bc9b5dc432e985cd0444
Andy Weiss <andyweiss1982@gmail.com>
2017-11-13 14:16:52 -0500

    update log in specs

--- spec/system/user_logs_in_spec.rb
+++ spec/system/user_logs_in_spec.rb
@@ -1,14 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "user logs in", type: :system, js: true do
-  context "with a valid email and password" do
-    it "redirects the user to his dashboard" do
-      user = create_user(email: "user@example.com", password: "password")
-      sign_in_as(email: user.email, password: "password")
-      expect(page).to have_content("Signed in successfully.")
+  context "with a valid email" do
+    context "and a valid password" do
+      it "redirects the user to his dashboard" do
+        user = create_user(email: "user@example.com", password: "password")
+        sign_in_as(email: user.email, password: "password")
+        expect(page).to have_content("Signed in successfully.")
+      end
+    end
+    context "with an invalid password" do
+      it "shows the user an error" do
+        user = create_user(email: "user@example.com", password: "password")
+        sign_in_as(email: user.email, password: "badpassword")
+        expect(page).to have_content("Invalid Email or Password")
+      end
     end
   end
-  context "with an invalid password" do
+  context "with an invalid email" do
     it "shows the user an error" do
       sign_in_as(email: "nonexistentuser@example.com", password: "password")
       expect(page).to have_content("Invalid Email or Password")
